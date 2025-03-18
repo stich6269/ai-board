582ebe420aedc84daa8311cf6174910c69933331
Polishchuk Artem <stich6269@gmail.com>
2025-03-18 12:31:54 +0200

    Loading logic improvement

--- src/app/pages/Statistic/components/Overview/Overview.tsx
+++ src/app/pages/Statistic/components/Overview/Overview.tsx
@@ -8,8 +8,8 @@ import {useEngineers} from "@models/engeneers";
 import {useIssues} from "@models/issues";
 
 export const Overview = memo(() => {
-  const {data, isFetching} = useIssues();
-  const {data: engineers, isFetching: isTeamLoading} = useEngineers();
+  const {data, isLoading} = useIssues();
+  const {data: engineers, isLoading: isTeamLoading} = useEngineers();
   const duration = useMemo(() => countDuration(data), [data])
   const {aiUsageDuration, aiBoostPercentage} = useMemo(() => aiIssuesStat(data), [data])
   
@@ -17,7 +17,7 @@ export const Overview = memo(() => {
     <OverviewStyled>
       <Stack direction="row" spacing={2}>
         <DemoPaper variant="elevation" data-testid="issues-block">
-          {isFetching &&   <Loading />}
+          {isLoading &&   <Loading />}
           <Title>Issues closed</Title>
           <Value>{data?.length || 0}</Value>
         </DemoPaper>
@@ -29,19 +29,19 @@ export const Overview = memo(() => {
         </DemoPaper>
         
         <DemoPaper variant="elevation" data-testid="working-time-block">
-          {isFetching &&   <Loading />}
+          {isLoading &&   <Loading />}
           <Title>Working time</Title>
           <Value>{duration || 0}h</Value>
         </DemoPaper>
         
         <DemoPaper variant="elevation" data-testid="ai-used-block">
-          {isFetching &&   <Loading />}
+          {isLoading &&   <Loading />}
           <Title>Ai used</Title>
           <Value>{aiUsageDuration || 0} h</Value>
         </DemoPaper>
         
         <DemoPaper variant="elevation" data-testid="ai-boost-block">
-          {isFetching &&   <Loading />}
+          {isLoading &&   <Loading />}
           <Title>AI usage boost</Title>
           <Value>{aiBoostPercentage || 0}%</Value>
         </DemoPaper>
